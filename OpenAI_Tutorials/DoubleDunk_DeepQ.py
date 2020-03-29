ceb0d57ecc35b319f26ae173b2aaefb42b10d257
Jordan Garrett <jordangarrett@ucsb.edu>
2020-03-29 10:51:47 -0700

    First Commit

---
+++ OpenAI_Tutorials/DoubleDunk_DeepQ.py
@@ -0,0 +1,373 @@
+import retro
+import gym
+import numpy as np
+import tensorflow as tf
+import keras.backend.tensorflow_backend as backend
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Conv2D, MaxPooling2D, Activation, Flatten, ZeroPadding2D, GlobalAveragePooling2D
+from keras.layers.normalization import BatchNormalization
+from keras.optimizers import Adam
+from keras.callbacks import ModelCheckpoint,ReduceLROnPlateau,EarlyStopping,TensorBoard,TerminateOnNaN
+from keras.layers.advanced_activations import LeakyReLU
+from collections import deque
+import time
+import random
+from tqdm import tqdm
+import os
+from PIL import Image
+import cv2
+import runai.ga 
+import keras.backend as K
+from keras.legacy import interfaces
+from keras.optimizers import Optimizer
+
+
+#for looking at tensorboard 
+#tensorboard --logdir logs/fit --host 127.0.0.1
+
+#Settings for the Deep Q learning
+DISCOUNT = 0.99
+REPLAY_MEMORY_SIZE = 10 #50_000  #50_000 How many last steps to keep for model training
+MIN_REPLAY_MEMORY_SIZE = 4#1000  #1_000 Minimum number of steps in a memory to start training
+MINIBATCH_SIZE = 1  #64 How many steps (samples) to use for training
+ACCUM_ITERS = 5 #64
+UPDATE_TARGET_EVERY = 2 #5  # Terminal states (end of episodes)
+model_name = '2x256'
+MIN_REWARD = -200  # For model save
+MEMORY_FRACTION = 0.20
+
+# Environment settings
+EPISODES = 3 #10_000
+
+# Exploration settings
+epsilon = 1  # not a constant, going to be decayed
+EPSILON_DECAY = 0.99975
+MIN_EPSILON = 0.001
+
+#  Stats settings
+AGGREGATE_STATS_EVERY = 50  # episodes
+SHOW_PREVIEW = False
+
+# Create models folder
+if not os.path.isdir('models'):
+    os.makedirs('models')
+    
+#Gradient accumulation optimizer, run batch size of 8
+#and accumulate over 8 steps to get batch of 64?
+#GA_optimizer = runai.ga.keras.optimizers.Adam(steps=STEPS)   
+
+    
+# Own Tensorboard class
+class ModifiedTensorBoard(TensorBoard):
+
+    # Overriding init to set initial step and writer (we want one log file for all .fit() calls)
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.step = 1
+        self.writer = tf.summary.FileWriter(self.log_dir)
+
+    # Overriding this method to stop creating default log writer
+    def set_model(self, model):
+        pass
+
+    # Overrided, saves logs with our step number
+    # (otherwise every .fit() will start writing from 0th step)
+    def on_epoch_end(self, epoch, logs=None):
+        self.update_stats(**logs)
+
+    # Overrided
+    # We train for one batch only, no need to save anything at epoch end
+    def on_batch_end(self, batch, logs=None):
+        pass
+
+    # Overrided, so won't close writer
+    def on_train_end(self, _):
+        pass
+
+    # Custom method for saving own metrics
+    # Creates writer, writes custom metrics and closes writer
+    def update_stats(self, **stats):
+        self._write_logs(stats, self.step)
+        
+        
+class AdamAccumulate(Optimizer):
+
+    def __init__(self, lr=0.001, beta_1=0.9, beta_2=0.999,
+                 epsilon=None, decay=0., amsgrad=False, accum_iters=1, **kwargs):
+        if accum_iters < 1:
+            raise ValueError('accum_iters must be >= 1')
+        super(AdamAccumulate, self).__init__(**kwargs)
+        with K.name_scope(self.__class__.__name__):
+            self.iterations = K.variable(0, dtype='int64', name='iterations')
+            self.lr = K.variable(lr, name='lr')
+            self.beta_1 = K.variable(beta_1, name='beta_1')
+            self.beta_2 = K.variable(beta_2, name='beta_2')
+            self.decay = K.variable(decay, name='decay')
+        if epsilon is None:
+            epsilon = K.epsilon()
+        self.epsilon = epsilon
+        self.initial_decay = decay
+        self.amsgrad = amsgrad
+        self.accum_iters = K.variable(accum_iters, K.dtype(self.iterations))
+        self.accum_iters_float = K.cast(self.accum_iters, K.floatx())
+
+    @interfaces.legacy_get_updates_support
+    def get_updates(self, loss, params):
+        grads = self.get_gradients(loss, params)
+        self.updates = [K.update_add(self.iterations, 1)]
+
+        lr = self.lr
+
+        completed_updates = K.cast(K.tf.floordiv(self.iterations, self.accum_iters), K.floatx())
+
+        if self.initial_decay > 0:
+            lr = lr * (1. / (1. + self.decay * completed_updates))
+
+        t = completed_updates + 1
+
+        lr_t = lr * (K.sqrt(1. - K.pow(self.beta_2, t)) / (1. - K.pow(self.beta_1, t)))
+
+        # self.iterations incremented after processing a batch
+        # batch:              1 2 3 4 5 6 7 8 9
+        # self.iterations:    0 1 2 3 4 5 6 7 8
+        # update_switch = 1:        x       x    (if accum_iters=4)  
+        update_switch = K.equal((self.iterations + 1) % self.accum_iters, 0)
+        update_switch = K.cast(update_switch, K.floatx())
+
+        ms = [K.zeros(K.int_shape(p), dtype=K.dtype(p)) for p in params]
+        vs = [K.zeros(K.int_shape(p), dtype=K.dtype(p)) for p in params]
+        gs = [K.zeros(K.int_shape(p), dtype=K.dtype(p)) for p in params]
+
+        if self.amsgrad:
+            vhats = [K.zeros(K.int_shape(p), dtype=K.dtype(p)) for p in params]
+        else:
+            vhats = [K.zeros(1) for _ in params]
+
+        self.weights = [self.iterations] + ms + vs + vhats
+
+        for p, g, m, v, vhat, tg in zip(params, grads, ms, vs, vhats, gs):
+
+            sum_grad = tg + g
+            avg_grad = sum_grad / self.accum_iters_float
+
+            m_t = (self.beta_1 * m) + (1. - self.beta_1) * avg_grad
+            v_t = (self.beta_2 * v) + (1. - self.beta_2) * K.square(avg_grad)
+
+            if self.amsgrad:
+                vhat_t = K.maximum(vhat, v_t)
+                p_t = p - lr_t * m_t / (K.sqrt(vhat_t) + self.epsilon)
+                self.updates.append(K.update(vhat, (1 - update_switch) * vhat + update_switch * vhat_t))
+            else:
+                p_t = p - lr_t * m_t / (K.sqrt(v_t) + self.epsilon)
+
+            self.updates.append(K.update(m, (1 - update_switch) * m + update_switch * m_t))
+            self.updates.append(K.update(v, (1 - update_switch) * v + update_switch * v_t))
+            self.updates.append(K.update(tg, (1 - update_switch) * sum_grad))
+            new_p = p_t
+
+            # Apply constraints.
+            if getattr(p, 'constraint', None) is not None:
+                new_p = p.constraint(new_p)
+
+            self.updates.append(K.update(p, (1 - update_switch) * p + update_switch * new_p))
+        return self.updates
+
+    def get_config(self):
+        config = {'lr': float(K.get_value(self.lr)),
+                  'beta_1': float(K.get_value(self.beta_1)),
+                  'beta_2': float(K.get_value(self.beta_2)),
+                  'decay': float(K.get_value(self.decay)),
+                  'epsilon': self.epsilon,
+                  'amsgrad': self.amsgrad}
+        base_config = super(AdamAccumulate, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
+
+
+       
+class DQAgent:
+    
+    def __init__(self):
+
+        # Main model
+        self.model = self.create_model()
+
+        # Target network
+        self.target_model = self.create_model()
+        self.target_model.set_weights(self.model.get_weights())
+
+        # An array with last n steps for training
+        self.replay_memory = deque(maxlen=REPLAY_MEMORY_SIZE)
+
+        # Custom tensorboard object
+        self.tensorboard = ModifiedTensorBoard(log_dir="logs/{}-{}".format(model_name, int(time.time())))
+
+        # Used to count when to update target network with main network's weights
+        self.target_update_counter = 0
+
+    def create_model(self):
+        model = Sequential()
+
+        model.add(Conv2D(256, (3, 3), input_shape=np.shape(env.observation_space)))  # observation_space = (250,160,3) a 250x160 RGB image.
+        model.add(Activation('relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        #model.add(Dropout(0.2))
+        model.add(BatchNormalization(axis=-1))
+
+        model.add(Conv2D(256, (3, 3)))
+        model.add(Activation('relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(BatchNormalization(axis=-1))
+        #model.add(Dropout(0.2))
+
+        model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
+        model.add(Dropout(0.2))
+        model.add(Dense(64))
+        model.add(Activation('relu'))
+        model.add(BatchNormalization())
+
+        model.add(Dense(env.action_space.n, activation='linear'))  # env.action_space.n = how many choices (8)
+        #model.compile(loss="mse", optimizer=Adam(lr=0.001), metrics=['accuracy']) #CPU
+        opt = AdamAccumulate(lr=0.001, decay=1e-5, accum_iters=ACCUM_ITERS)
+        model.compile(loss="mse", optimizer=opt, metrics = ['accuracy'])
+        return model
+
+    # We are fitting on every observation, so to prevent overgeneralization we can train on batches of previous observations,
+    # which we store in replay_memory
+    def update_replay_memory(self, transition):
+        self.replay_memory.append(transition)
+        
+        
+    # Trains main network every step during episode
+    def train(self, terminal_state, step):
+
+        # Start training only if certain number of samples is already saved
+        if len(self.replay_memory) < MIN_REPLAY_MEMORY_SIZE:
+            return
+
+        # Get a minibatch of random samples from memory replay table
+        minibatch = random.sample(self.replay_memory, MINIBATCH_SIZE)
+
+        # Get current states from minibatch, then query NN model for Q values
+        current_states = np.array([transition[0] for transition in minibatch])/255
+        current_qs_list = self.model.predict(current_states)
+
+        # Get future states from minibatch, then query NN model for Q values
+        # When using target network, query it, otherwise main network should be queried
+        new_current_states = np.array([transition[3] for transition in minibatch])/255
+        future_qs_list = self.target_model.predict(new_current_states)
+
+        X = []
+        y = []
+
+        # Now we need to enumerate our batches
+        for index, (current_state, action, reward, new_current_state, done) in enumerate(minibatch):
+
+            # If not a terminal state, get new q from future states, otherwise set it to 0
+            # almost like with Q Learning, but we use just part of equation here
+            if not done:
+                max_future_q = np.max(future_qs_list[index])
+                new_q = reward + DISCOUNT * max_future_q
+            else:
+                new_q = reward
+
+            # Update Q value for given state
+            current_qs = current_qs_list[index]
+            current_qs[action] = new_q
+
+            # And append to our training data
+            X.append(current_state)
+            y.append(current_qs)
+
+        # Fit on all samples as one batch, log only on terminal state
+        self.model.fit(np.array(X)/255, np.array(y), batch_size=MINIBATCH_SIZE, verbose=0, shuffle=False, 
+                       callbacks=[self.tensorboard] if terminal_state else None)
+
+        # Update target network counter every episode
+        if terminal_state:
+            self.target_update_counter += 1
+
+        # If counter reaches set value, update target network with weights of main network
+        if self.target_update_counter > UPDATE_TARGET_EVERY:
+            self.target_model.set_weights(self.model.get_weights())
+            self.target_update_counter = 0
+
+    # Queries main network for Q values given current observation space (environment state)
+    def get_qs(self, state):
+        return self.model.predict(np.array(state).reshape(-1, *state.shape)/255)[0]
+    
+
+
+
+
+
+
+
+
+    
+env = retro.make(game = 'DoubleDunk-Atari2600')
+
+#always run before using a model
+backend.clear_session()
+tf.compat.v1.reset_default_graph()
+agent = []
+agent = DQAgent()
+
+# Iterate over episodes
+for episode in tqdm(range(1, EPISODES + 1), ascii=True, unit='episodes'):
+
+    # Update tensorboard step every episode
+    agent.tensorboard.step = episode
+
+    # Restarting episode - reset episode reward and step number
+    episode_reward = 0
+    step = 1
+
+    # Reset environment and get initial state
+    current_state = env.reset()
+
+    # Reset flag and start iterating until episode ends
+    done = False
+    while not done:
+
+        # This part stays mostly the same, the change is to query a model for Q values
+        if np.random.random() > epsilon:
+            # Get action from Q table
+            action = np.argmax(agent.get_qs(current_state))
+        else:
+            # Get random action
+            action = np.random.randint(0, 2, size = env.action_space.n) #actionspace is MultiBinary(8)
+
+        new_state, reward, done, info = env.step(action)
+
+        # Transform new continous state to new discrete state and count reward
+        episode_reward += reward
+        
+        # % is called a modulus. This divides and returns the remainder. So if the remainder is 0, then render
+        #if SHOW_PREVIEW and not episode % AGGREGATE_STATS_EVERY:
+        env.render()
+
+        # Every step we update replay memory and train main network
+        agent.update_replay_memory((current_state, action, reward, new_state, done))
+        agent.train(done, step)
+
+        current_state = new_state
+        step += 1
+
+    # Append episode reward to a list and log stats (every given number of episodes)
+    ep_rewards.append(episode_reward)
+    if not episode % AGGREGATE_STATS_EVERY or episode == 1:
+        average_reward = sum(ep_rewards[-AGGREGATE_STATS_EVERY:])/len(ep_rewards[-AGGREGATE_STATS_EVERY:])
+        min_reward = min(ep_rewards[-AGGREGATE_STATS_EVERY:])
+        max_reward = max(ep_rewards[-AGGREGATE_STATS_EVERY:])
+        agent.tensorboard.update_stats(reward_avg=average_reward, reward_min=min_reward, reward_max=max_reward, epsilon=epsilon)
+
+        # Save model, but only when min reward is greater or equal a set value
+        if min_reward >= MIN_REWARD:
+            agent.model.save(f'models/{model_name}__{max_reward:_>7.2f}max_{average_reward:_>7.2f}avg_{min_reward:_>7.2f}min__{int(time.time())}.model')
+
+    # Decay epsilon
+    if epsilon > MIN_EPSILON:
+        epsilon *= EPSILON_DECAY
+        epsilon = max(MIN_EPSILON, epsilon)
+        
+env.close()
